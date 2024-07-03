199bfb5ad38d1457d7444643aefbc6a61d447970
Ccode25 <ajdaelo25@gmail.com>
2024-07-04 00:16:14 +0800

    added media query

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ $("#About").click(function() {
       scrollTop: $("#myDiv").offset().top
   }, 100);
 });
+
+// $(document).ready(function() {
+//   var navbar = $('.nav-bar');
+//   var lastScrollTop = 0;
+
+//   $(window).scroll(function() {
+//     var scrollTop = $(this).scrollTop();
+
+//     if (scrollTop > lastScrollTop) {
+//       // Downscroll
+//       navbar.css('transform', 'translateY(-100%)'); // Hide navbar
+//     } else {
+//       // Upscroll
+//       navbar.css('transform', 'translateY(0)'); // Show navbar
+//     }
+
+//     lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+//   });
+// });
